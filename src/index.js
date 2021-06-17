a530a9c6b0f22fdfa2a448d3751b5ec83ba2ed17
tbcurtis <tbcurtis20@gmail.com>
2021-06-16 21:33:11 -0700

    setup routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-console.log('Hello')
-let test = () => {
-    console.log('test');
-}
-test();
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("test");
+});
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
