d32232d7eb10baa3dba21c1e471192ba4cbccfea
tbcurtis <tbcurtis20@gmail.com>
2021-06-20 02:01:27 -0700

    Added acceptRequest and getRequest post routes with player specification

--- src/index.js
+++ src/index.js
@@ -45,30 +45,62 @@ router.get("/getAllOpenRequests", (req, res) => {
     })
     .catch((e) => console.error(e.stack));
 });
-router.get("/getRequest", (req, res) => {
+
+router.get("/getRequest/:id", (req, res) => {
+  //console.log(req);
   pool
-    .query(`SELECT * FROM request where(id = $1)`)
+    .query(`SELECT * FROM request where(id = $1)`, [req.params.id])
     .then((results) => {
-      console.table(results.rows), res.send(results.rows[0]);
+      if (results.rows != null) {
+        console.table(results.rows), res.send(results.rows);
+      } else {
+        res.send({});
+      }
     })
     .catch((e) => console.error(e.stack));
 });
+
 router.post("/createRequest", (req, res) => {
   let { creator } = req.body;
   //console.log(req);
   pool
     .query(`INSERT INTO request(creator,createdOn) values($1,NOW())`, [creator])
     .then((results) => {
-      res.status(201).send({message:"Insertion successful"})
+      res.status(201).send({ message: "Insertion successful" });
       //console.table(results), res.send(results);
     })
     .catch((e) => console.error(e.stack));
 });
+
 router.post("/acceptRequest", (req, res) => {
+  let { id, playername } = req.body;
+  let player = "";
   pool
-    .query(`UPDATE request where ($1 = id) set $????? = $2`)
-    .then((results) => {
-      console.table(results.rows), res.send(results.rows[0]);
+    .query(
+      `SELECT * FROM request where(id = $1 and (player1 is null or player2 is null or player3 is null))`,
+      [id]
+    )
+    .then((results1) => {
+      if (results1.rowCount === 0) {
+        res.status(404).send({ message: "request not found" });
+      } else {
+        if (results1.rows[0].player1 === null) {
+          player = "player1";
+        } else if (results1.rows[0].player2 === null) {
+          player = "player2";
+        } else if (results1.rows[0].player3 === null) {
+          player = "player3";
+        }
+        pool
+          .query(`UPDATE request set ${player} = $1::text where id = $2`, [
+            playername,
+            id,
+          ])
+          .then((results) => {
+            res.status(201).send({ message: `Sucessfully added ${player}` });
+          })
+          .catch((e) => console.error(e.stack));
+      }
     })
     .catch((e) => console.error(e.stack));
 });
