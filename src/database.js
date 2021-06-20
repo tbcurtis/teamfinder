1b4f3a86b8371780551cbe823474a55f1c351b06
tbcurtis <tbcurtis20@gmail.com>
2021-06-20 00:48:34 -0700

    Added create and getAllRequests routes

--- src/database.js
+++ src/database.js
@@ -36,14 +36,35 @@
 // }
 
 // export test;
+// const { Client } = require("pg");
+
+// const client = new Client({
+//   user: "bjnmc",
+//   password: "password",
+//   host: "localhost",
+//   port: 5432,
+//   database: "teamfinder",
+// });
+// client
+//   .connect()
+//   .then(() => console.log("Connected successfully"))
+//   .then(() => client.query("select * from request"))
+//   .then(results => console.table(results.rows))
+//   .catch((e) => console.error(e))
+//   .finally(() => client.end());
+
+// export default client;
+
+//NEED TO DO " pg_ctl start  " !!!!!!!
 
 import { Pool } from "pg";
 
 const pool = new Pool({
   host: "localhost",
   user: "postgres",
+  database: "teamfinder",
   max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  //idleTimeoutMillis: 30000,
+  //connectionTimeoutMillis: 2000,
 });
 export default pool;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import pool from "./database";
 
 const app = express();
 const router = express.Router();
 const port = 3000;
+
+app.use(json()); //use middleware
 
 //run db migration
 pool
@@ -14,57 +16,65 @@ pool
     player1 text,
     player2 text,
     player3 text,
-    createdOn TIMESTAMP NOT NULL)`
+    createdOn TIMESTAMP NOT NULL);`
   )
   .then((dbres) => {
-    console.log(dbres.rows[0]);
+    console.table(dbres.rows);
   })
   .catch((e) => console.error(e.stack));
 
 router.get("/", (req, res) => {
   pool
     .query("SELECT NOW() as now")
-    .then((dbres) => {
-      res.send(dbres.rows[0]);
+    .then((results) => {
+      console.table(results.rows), res.send(results.rows[0]);
     })
     .catch((e) => console.log(e.stack));
 });
 router.get("/getAllOpenRequests", (req, res) => {
   pool
     .query(
-      `SELECT * FROM request where( player1 is not null or player2 is not null or player3 is not null)`
+      `SELECT * FROM request where( player1 is null or player2 is null or player3 is null);`
     )
-    .then((dbres) => {
-      res.send(dbres.rows[0]);
+    .then((results) => {
+      if (results.rows != null) {
+        console.table(results.rows), res.send(results.rows);
+      } else {
+        res.send({});
+      }
     })
     .catch((e) => console.error(e.stack));
 });
 router.get("/getRequest", (req, res) => {
   pool
     .query(`SELECT * FROM request where(id = $1)`)
-    .then((dbres) => {
-      res.send(dbres.rows[0]);
+    .then((results) => {
+      console.table(results.rows), res.send(results.rows[0]);
     })
     .catch((e) => console.error(e.stack));
 });
 router.post("/createRequest", (req, res) => {
+  let { creator } = req.body;
+  //console.log(req);
   pool
-    .query(`INSERT INTO request values($creator,$player1,$player2,$player3)`)
-    .then((dbres) => {
-      res.send(dbres.rows[0]);
+    .query(`INSERT INTO request(creator,createdOn) values($1,NOW())`, [creator])
+    .then((results) => {
+      res.status(201).send({message:"Insertion successful"})
+      //console.table(results), res.send(results);
     })
     .catch((e) => console.error(e.stack));
 });
 router.post("/acceptRequest", (req, res) => {
   pool
     .query(`UPDATE request where ($1 = id) set $????? = $2`)
-    .then((dbres) => {
-      res.send(dbres.rows[0]);
+    .then((results) => {
+      console.table(results.rows), res.send(results.rows[0]);
     })
     .catch((e) => console.error(e.stack));
 });
 
 app.use("/api", router);
+
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
