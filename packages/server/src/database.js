fa535c50430ec4ce2bf0d00dc0c208842f73d73e
tbcurtis <tbcurtis20@gmail.com>
2021-06-25 18:57:37 -0700

    added database

--- packages/server/src/database.js
+++ packages/server/src/database.js
@@ -1,9 +1,9 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: "localhost",
+  host: "ec2-35-170-85-206.compute-1.amazonaws.com",
   user: "postgres",
-  database: "teamfinder",
+  database: "d501hafk566d57",
   max: 20,
 });
 export default pool;
